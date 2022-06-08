38445a561d66eadf42e3660aad44d119ce5237db
xbt573 <alunev5@yandex.ru>
2022-06-08 13:30:11 +0300

    fixed lint

--- src/commands/base64decode.js
+++ src/commands/base64decode.js
@@ -24,7 +24,7 @@ const command = async (ctx) => {
         await ctx.reply(base64,
             { reply_to_message_id: ctx.message.message_id });
     } catch {
-	await ctx.replyWithMarkdown('*Base64 string is incorrect!*');
+        await ctx.replyWithMarkdown('*Base64 string is incorrect!*');
     }
 };
 
